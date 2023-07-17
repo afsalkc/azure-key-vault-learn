2eeddd67659ca3d7f2f54fe2505ff9d7b45e8697
AFSAL KC <nkcafsal@gmail.com>
2023-07-17 11:07:35 +0000

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace dotnetcore
 {
@@ -6,7 +9,23 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "MySecret";
+            string KeyVaultName = "learn-keyvault5424";
+            var kvUri = "https://learn-keyvault5424.vault.azure.net";
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+            Console.WriteLine(secret.Value);
+
         }
     }
 }
